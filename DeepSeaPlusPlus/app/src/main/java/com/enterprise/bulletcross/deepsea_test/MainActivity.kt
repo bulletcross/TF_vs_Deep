13c0d69dfda887112af41416c6a71e53f56b25ce
bulletcross <bulletcross@gmail.com>
2017-10-14 21:40:09 +0530

    support for other libs

--- DeepSeaPlusPlus/app/src/main/java/com/enterprise/bulletcross/deepsea_test/MainActivity.kt
+++ DeepSeaPlusPlus/app/src/main/java/com/enterprise/bulletcross/deepsea_test/MainActivity.kt
@@ -25,6 +25,8 @@ class MainActivity : AppCompatActivity() {
         // Used to load the 'native-lib' library on application startup.
         init {
             System.loadLibrary("native-lib")
+            System.loadLibrary("blaze_target")
+
         }
     }
 }
