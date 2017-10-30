481ed0f5a37a8de62828b23aab5752ef5f9d4091
bulletcross <bulletcross@gmail.com>
2017-10-30 22:40:08 +0530

    added other classes

--- DeepSeaPlusPlus/app/src/main/java/com/enterprise/bulletcross/deepsea_test/MainActivity.kt
+++ DeepSeaPlusPlus/app/src/main/java/com/enterprise/bulletcross/deepsea_test/MainActivity.kt
@@ -9,15 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val input_vector = DoubleArray(4)
-        input_vector[0] = 3.4
-        input_vector[1] = 6.7
-        input_vector[2] = 8.9
-        input_vector[3] = 3.0
-        // Example of a call to a native method
-        val max_from_jni:Double = dummy(input_vector)
-        //println(dummy(input_vector))
-        sample_text.text = stringFromJNI() + max_from_jni.toString()
     }
 
     /**
---
+++ DeepSeaPlusPlus/app/src/main/java/com/enterprise/bulletcross/deepsea_test/inference/predictor.kt
@@ -0,0 +1,7 @@
+package com.enterprise.bulletcross.deepsea_test.inference
+
+/**
+ * Created by vishal on 30/10/17.
+ */
+class predictor {
+}
